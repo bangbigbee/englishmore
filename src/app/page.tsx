36649adfb76e44328039599c6f0d5d3d22980c95
bangbigbee <bangdtbk@gmail.com>
2026-03-30 15:54:28 +0700

    Fix TypeScript error: setMemberCourseTitle not defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
   useEffect(() => {
     if (!session) {
       setAvailableCourses([])
-      setMemberCourseTitle('')
+      setMemberEnrollment(null)
       return
     }
 
