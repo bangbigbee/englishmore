317fd3a3fca49a19950e1c4fd4aed0de88576798
bangbigbee <bangdtbk@gmail.com>
2026-04-30 10:12:20 +0700

    style(theme): match ocean-blue perfectly with dictation practice button color (#219AFF)

---
+++ scratch/get-color.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+// Very simple PNG parser just to get a general idea, or better yet, we can use an npm package!
+// We are in a project with package.json, we can use `npx jimp` or similar, but maybe we can just install a temp package.
